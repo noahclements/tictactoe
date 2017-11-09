a5394390a397d8d4a08bc261323d43777d4d6a62
Noah Clements <noahnoah900@gmail.com>
2017-11-09 11:38:06 -0400

    added grid initializer method, WIP

--- tictactoe/src/tictactoe.java
+++ tictactoe/src/tictactoe.java
@@ -1,31 +1,52 @@
+import java.util.Scanner;
 
 public class tictactoe {
-	public static void main(String[] args) {
+	private static char[][] grid = new char[3][3];
 
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String input = scanner.nextLine();
+		
 		System.out.println("Player vs Player (A)");
 		System.out.println("Player vs AI (B)");
-		
-		gridMaker();
 
+		if(input.equalsIgnoreCase("A")) {
+			
+		} else if(input.equalsIgnoreCase("B")) {
+			
+		}
+	}
+
+	public static void gridInitialize() {
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				grid[i][j] = '-';
+			}
+		}
 	}
 
 	public static void gridMaker() {
-	
-		char[][] grid;
-		grid = new char[3][3];
 
-		System.out.println("  A   B   C");
+		System.out.println("  A   B   C ");
 		System.out.println("-------------");
-		
-        for (int i = 0; i < 3; i++) {
-            System.out.print("| ");
-            for (int j = 0; j < 3; j++) {
-                System.out.print(grid[i][j] + " | ");
-            }
-            System.out.print(i+1);
-            System.out.println("\n" + "-------------");
-        }
-		
+
+		for (int i = 0; i < 3; i++) {
+			System.out.print("| ");
+			for (int j = 0; j < 3; j++) {
+				System.out.print(grid[i][j] + " | ");
+			}
+			System.out.print(i + 1);
+			System.out.println("\n" + "-------------");
+		}
+
+	}
+	
+	public static void placeMark() {
 		
 	}
+	
+	
+	
+	
+	
 }
