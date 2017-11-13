b78ef747eae1c33517e0c7d88d38513258981529
Noah Clements <noahnoah900@gmail.com>
2017-11-13 01:16:24 -0400

    placeMark function works,
    app currently crashes when you try to put use areaPicker the 2nd time,
    to be fixed tomorrow..

--- tictactoe/src/tictactoe.java
+++ tictactoe/src/tictactoe.java
@@ -51,24 +51,27 @@ public class tictactoe {
 		System.out.println("Pick a row (A-C)");
 		String input = scanner.nextLine();
 		if(input.equalsIgnoreCase("A")) {
-			rowPicked = 1;		// A = 1, etc.
+			rowPicked = 0;		// A = 0, etc.
 		} else if(input.equalsIgnoreCase("B")) {
-			rowPicked = 2;
+			rowPicked = 1;
 		} else if(input.equalsIgnoreCase("C")) {
-			rowPicked = 3;
+			rowPicked = 2;
 		}
 		
 		System.out.println("Pick a column (1-3)");
 		int numInput = scanner.nextInt();
 		if(numInput == 1) {
-			columnPicked = 1;
-			placeMark();
+			columnPicked = 0;	// grid array starts at 0..
+			markerChange();
+			placeMark(columnPicked, rowPicked);
 		} else if(numInput == 2) {
-			columnPicked = 2;
-			placeMark();
+			columnPicked = 1;
+			markerChange();
+			placeMark(columnPicked, rowPicked);
 		} else if(numInput == 3) {
-			columnPicked = 3;
-			placeMark();
+			columnPicked = 2;
+			markerChange();
+			placeMark(columnPicked, rowPicked);
 		}
 	}
 	
@@ -83,10 +86,11 @@ public class tictactoe {
 	}
 	
 	
-	public static void placeMark() {
-		if(grid[columnPicked][rowPicked] == '-') {
-			grid[columnPicked][rowPicked] = marker;
+	public static void placeMark(int rowPicked, int columnPicked) {
+		if(grid[rowPicked][columnPicked] == '-') {
+			grid[rowPicked][columnPicked] = marker;
 		}
+		gridMaker();
 	}
 		
 	
